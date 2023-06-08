37e396cda6af40e219503a26b23851b7c1fe44b0
ArtemUb <savenkoam@gmail.com>
2023-06-08 19:50:15 +0900

    Task 64 is Done

--- task64/Program.cs
+++ task64/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+int Promt(string msg)
+{
+    System.Console.WriteLine(msg);
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+string AllNumbers(int num)
+{
+    string res = Convert.ToString(num);
+    if (num == 1) return res;
+    return res + ", " + AllNumbers(num - 1);
+}
+
+
+
+int num = Promt("Enter a number:");
+System.Console.WriteLine(AllNumbers(num));

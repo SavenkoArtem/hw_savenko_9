26162a7b383fe83a2e01ae9fdbd7f7fe480f4247
ArtemUb <savenkoam@gmail.com>
2023-06-09 09:05:40 +0900

    Task 68 is Done

--- task68/Program.cs
+++ task68/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 66: Задайте значения M и N. Напишите программу,
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int Promt(string msg)
+{
+    System.Console.WriteLine(msg);
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+int FindAckerman(int m, int n)
+{    
+    if (m == 0) return n + 1;
+    if (n == 0) return FindAckerman(m - 1, 1);
+    return FindAckerman(m - 1, FindAckerman(m, n - 1));
+}
+
+int m = Promt("Enter a number m:");
+int n = Promt("Enter a number n:");
+System.Console.WriteLine(FindAckerman(m, n));
